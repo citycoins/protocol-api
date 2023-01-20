930ed51b70357e57583696f91674317c713afc0f
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-01-20 14:41:23 -0700

    fix: minor adjustments to config

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
-import reactLogo from './assets/react.svg';
 import './App.css';
-
+import ccLogo from './assets/citycoins.png';
 import * as MicroStacks from '@micro-stacks/react';
 import { WalletConnectButton } from './components/wallet-connect-button';
 import { UserCard } from './components/user-card';
-import { Logo } from './components/ustx-logo';
 
 function Contents() {
   return (
     <>
-      <div className={'logos'}>
-        <a
-          href="https://micro-stacks.dev"
-          target="_blank"
-          className={'micro-stacks-logo'}
-        >
-          <Logo
-            size={84}
-            className="logo"
-          />
-        </a>
-        <a
-          href="https://vitejs.dev"
-          target="_blank"
-        >
-          <img
-            src="/vite.svg"
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-        >
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
       <h1>micro-stacks + Vite + React</h1>
       <div className="card">
         <UserCard />
@@ -64,8 +30,8 @@ function Contents() {
 export default function App() {
   return (
     <MicroStacks.ClientProvider
-      appName={'React + micro-stacks'}
-      appIconUrl={reactLogo}
+      appName={'CityCoins Protocol API'}
+      appIconUrl={ccLogo}
     >
       <Contents />
     </MicroStacks.ClientProvider>
