2fe15d5217c9ce423514709732aedcfe92fdb2b1
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-02-23 09:15:52 -0700

    feat: add ccd011 endpoint

--- functions/api/ccd011-stacking-payouts/get-pool-operator.ts
+++ functions/api/ccd011-stacking-payouts/get-pool-operator.ts
@@ -3,8 +3,29 @@ import { DEPLOYER, NETWORK } from '../../../lib/api-helpers';
 
 // TODO: upgrade types and check if EventContext is found
 export async function onRequest(context: any): Promise<Response> {
-  // check query parameters
   // get result from contract
+  const poolOperator = await getPoolOperator();
+
   // return result
-  return new Response('Not implemented, coming soon!', { status: 501 });
+  if (!poolOperator) return new Response(`Pool operator not found`, { status: 404 });
+  return new Response(JSON.stringify(poolOperator));
+}
+
+// returns the pool operator
+async function getPoolOperator() {
+  try {
+    const result = await fetchReadOnlyFunction(
+      {
+        contractAddress: DEPLOYER('mainnet'),
+        contractName: 'ccd011-stacking-payouts',
+        functionName: 'get-pool-operator',
+        functionArgs: [],
+        network: NETWORK('mainnet'),
+      },
+      true
+    );
+    return result ? String(result) : undefined;
+  } catch (err) {
+    return undefined;
+  }
 }
